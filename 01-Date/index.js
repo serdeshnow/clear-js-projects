c77221c474d24e7047a7464b260d86530b8706ee
serdeshnow <vserdes1977@mail.ru>
2024-08-10 19:17:39 +0300

    upd: upload date-project

---
+++ 01-Date/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+// import { App } from "./src/modules/app.js"; //NOT DEFAULT
+// import AnyNameAppBecauseDefault from "./src/modules/app.js"; //EXPORT DEFAULT
+import App from "./src/modules/app.js";
+
+const app = new App();
+app.run();
+
+console.log("__work__");
---
+++ 01-Date/src/core/constants/settings.js
@@ -0,0 +1,1 @@
+export const JS_CREATION_DATE = new Date(1995, 0, 1); // Jan 01 1995
